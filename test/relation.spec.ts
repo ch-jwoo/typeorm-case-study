0ac95b7565b06699014f600977692ac277d9c91a
junu <cjw8809@gmail.com>
2024-08-31 17:10:58 +0900

    Add test case for relation

---
+++ test/relation.spec.ts
@@ -0,0 +1,90 @@
+import { join } from 'path';
+import {
+  Column,
+  DataSource,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('content')
+class Content {
+  @PrimaryGeneratedColumn()
+  id!: string;
+
+  @Column()
+  name!: string;
+}
+
+@Entity('comment')
+class Comment {
+  @PrimaryGeneratedColumn()
+  id!: string;
+
+  @Column()
+  text!: string;
+
+  @ManyToOne(() => Content)
+  content: Content;
+}
+
+const dbPath = join('db', 'relation.sqlite');
+
+describe('Test', () => {
+  let dataSoure: DataSource;
+
+  let targetComment: Comment;
+  let targetContent: Content;
+
+  beforeAll(async () => {
+    dataSoure = new DataSource({
+      type: 'sqlite',
+      database: dbPath,
+      logging: true,
+      entities: [Content, Comment],
+      // 개발용
+      synchronize: true,
+      dropSchema: true,
+    });
+    await dataSoure.initialize();
+
+    const content = new Content();
+    content.name = 'content1';
+    await dataSoure.getRepository(Content).save(content);
+    targetContent = content;
+
+    const comment = new Comment();
+    comment.text = 'comment1';
+    comment.content = content;
+    await dataSoure.getRepository(Comment).save(comment);
+    targetComment = comment;
+  });
+
+  it('should be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should join', async () => {
+    const commentRepository = dataSoure.getRepository(Comment);
+    const comment = await commentRepository
+      .createQueryBuilder('comment')
+      .innerJoinAndSelect('comment.content', 'content')
+      .where('content.id = :id', { id: targetContent.id })
+      .getOne();
+    expect(comment).toEqual(targetComment);
+  });
+
+  it('should not join', async () => {
+    const contentRepository = dataSoure.getRepository(Content);
+    expect.hasAssertions();
+    try {
+      const content = await contentRepository
+        .createQueryBuilder('content')
+        .innerJoinAndSelect('content.comment', 'comment')
+        .where('comment.id = :id', { id: targetComment.id })
+        .getOne();
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
+});
